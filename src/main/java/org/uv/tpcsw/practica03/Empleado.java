a708a6d67e63859a06fc7c74eba1acc6d50cd70c
Samantha-Varguez <varaguezsamy@gmail.com>
2024-10-22 08:28:31 -0600

    "correcciones"

--- src/main/java/org/uv/tpcsw/practica03/Empleado.java
+++ src/main/java/org/uv/tpcsw/practica03/Empleado.java
@@ -4,12 +4,30 @@
  */
 package org.uv.tpcsw.practica03;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
         
         // todo pojo debe ser serializable
-public class Empleado {
+@Entity
+@Table(name = "empleados2")
+public class Empleado implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "empleados2_clave_seq")
+    @SequenceGenerator(name = "empleados2_clave_seq", sequenceName = "empleados2_clave_seq", initialValue = 1, allocationSize = 1)
+    @Column
      private String clave; 
+    @Column
     private String nombre;
+    @Column
     private String direccion;
+    @Column
     private String telefono;
 
     public String getClave() {
--- src/main/java/org/uv/tpcsw/practica03/TPCSWPractica03.java
+++ src/main/java/org/uv/tpcsw/practica03/TPCSWPractica03.java
@@ -7,13 +7,37 @@
 // resolver los otros metodos
 package org.uv.tpcsw.practica03;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class TPCSWPractica03 {
-
+// load es para lazy que se quiere volver eager
     public static void main(String[] args) {
         //hibernate 
         System.out.println("Hello World!");
+        Departamentos departamento = new Departamentos();
+        departamento.setNombre("Administracion");
         SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction t=session.beginTransaction();
+        session.save(departamento);
+        
+       t.commit();
+// load hace referencia
+//SessionFactory sf = HibernateUtil.getSessionFactory();
+//Session session = sf.getCurrentSession();
+//Transaction t = session.beginTransaction();
+//Departamento dep=session.get(Departamento.class, 1L);
+//
+//if (dep!= null){
+//    System.out.println("Clave" + dep.getClave()+ "nom"+ dep.getNombre());
+//Set<Empleado> 1stEmpleado = dep.getEmpleados();
+//for (Empleado enpleado1 = 1stEmpleado){
+//    System.out.println("clave" + empleado.getNombre);
+//}
+//}
+//        t.commit();
+        
     }
 }
